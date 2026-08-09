96b28744a4f32d6329d25ed0a0ab06ab28ac5954
BullerWilliam <166019658+BullerWilliam@users.noreply.github.com>
2026-08-09 17:19:34 +0200

    Add dev-panel homepage news content source

---
+++ apps/home/src/lib/content/dev-panel/homepage-news.js
@@ -0,0 +1,25 @@
+const homepageNews = {
+    title: "All The Mods Game Jam has ended!",
+    body: [
+        "Thanks for participating in the event! Rankings will be announced on itch.io. See you soon!"
+    ],
+    seeMoreHref: "https://itch.io/jam/all-the-mods-game-jam",
+    seeMoreLabel: "All The Mods on itch.io",
+    seeMoreExternal: true,
+    inlineLinks: [
+        {
+            label: "itch.io",
+            href: "https://itch.io/jam/all-the-mods-game-jam"
+        }
+    ],
+    image: {
+        src: "https://penguinmod.com/events/news/allthemods2026.webp?r=1",
+        alt: "All The Mods 2026"
+    },
+    footer: {
+        default: "TODO: UNIMPORTANT: This should be a fun fact",
+        untranslated: "DevCore News is not translated in your language. Sorry! :("
+    }
+};
+
+export default homepageNews;
