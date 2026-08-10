53f6f7b1a98744378f5abe59b13f94e50d290e81
BullerWilliam <166019658+BullerWilliam@users.noreply.github.com>
2026-08-10 09:58:16 +0200

    Point shared home links to owned credits and contact routes

--- apps/home/src/lib/resources/site-paths.js
+++ apps/home/src/lib/resources/site-paths.js
@@ -4,7 +4,7 @@ export const DEVCORE_EXTENSIONS_GALLERY_PATH = '/extensions-gallery/';
 export const DEVCORE_EDITOR_PATH = '/editor/';
 export const DEVCORE_PLAYER_PATH = '/player/';
 export const DEVCORE_PACKAGER_PATH = '/packager/';
-export const DEVCORE_CREDITS_PATH = '/editor/credits.html';
-export const DEVCORE_CONTACT_PATH = '/editor/contact.html';
+export const DEVCORE_CREDITS_PATH = '/credits/';
+export const DEVCORE_CONTACT_PATH = '/contact/';
 
 export const buildSharedProjectPath = projectId => `${DEVCORE_PLAYER_PATH}#${projectId}`;
